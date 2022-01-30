0fd3f78acbef8ee3d7bd2714a0398f332d6894c7
daksheshs <daksheshkhandal@gmail.com>
2022-01-30 13:16:53 +0530

    Prob-A: Keep Multiplying Found Values by Two

---
+++ Leetcode_Problems/Weekly_Contest_278/Prob-A.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        while(true) {
+            bool is_present = false;
+            for(int num: nums) {
+                if(num == original) {
+                    is_present = true;
+                    break;
+                }
+            }
+            if(!is_present)
+                break;
+            original *= 2;
+        }
+        
+        return original;
+    }
+};
